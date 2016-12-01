d3fe753d4062885164a6af5ff8a1da46d8b410a3
Vuong Nguyen <vuongnq.09@gmail.com>
2016-12-01 14:49:43 +0000

    Add log for server

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,17 +1,171 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Lib
-	(
-startApp
-	) where
+ (startApp)
+ where
 
-import Followers (showFollowers)
+import           Prelude ()
+import           Prelude.Compat
+
+import           Data.Aeson.Types
+import           Data.List
+import           Data.Maybe
+import           GHC.Generics
+
+import           Servant
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Network.Wai.Logger
+
+import           System.Log.Formatter
+import           System.Log.Handler           (setFormatter)
+import           System.Log.Handler.Simple
+import           System.Log.Handler.Syslog
+import           System.Log.Logger
+
+import           Control.Concurrent           (forkIO, threadDelay)
+import           Control.Monad                (when)
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Except   (ExceptT)
+
+import           System.Environment           (getArgs, getProgName, lookupEnv)
+import           Data.Time.Clock              (UTCTime, getCurrentTime)
+import           Data.Time.Format             (defaultTimeLocale, formatTime)
 
 import qualified GitHub as GH
 
-startApp = do
-    -- possibleUser <- GH.executeRequest' $ GH.userInfoForR "torvalds"
-    -- print possibleUser
-    showFollowers
-    
+type API = "hello" :> QueryParam "name" String :> Get '[JSON] HelloMessage
+      :<|> "post-repo-info" :> ReqBody '[JSON] [RepoInfo] :> Post '[JSON] RepoInfoResponse
+
+newtype HelloMessage = HelloMessage { msg :: String }
+  deriving Generic
+instance ToJSON HelloMessage
+
+data RepoInfo = RepoIno -- group 3
+    {   repo_name   :: String,
+        owner       :: String,
+        token       :: String
+    } deriving Generic
+instance ToJSON RepoInfo
+instance FromJSON RepoInfo
+
+data RepoInfoResponse = RepoInfoResponse -- response for group 3
+    {
+        success     :: Bool
+--         success     :: String
+    } deriving Generic
+instance ToJSON RepoInfoResponse
+
+-- Start server here
+startApp :: IO ()    -- set up wai logger for service to output apache style logging for rest calls
+startApp = withLogging $ \ aplogger -> do
+  warnLog "Starting use-haskell."
+
+  forkIO $ taskScheduler 5
+
+  let settings = setPort 8080 $ setLogger aplogger defaultSettings
+  runSettings settings app
+
+taskScheduler :: Int -> IO ()
+taskScheduler delay = do
+  warnLog $ "Task scheduler operating."
+
+  threadDelay $ delay * 1000000
+  taskScheduler delay -- tail recursion
+
+-- | the function app calls serve, passing api and server. You can see that there is a kind of structure of function
+-- composition here that is to do with how servant operates. The first parameter defines the type of the REST service,
+-- (which is defined as type `Proxy API` - yes types of this formt are allowed - and we should note that the second part
+-- of that type is out previously defined REST API) and the second parameter defines the implementation of the REST service.
+app :: Application
+app = serve api server
+
+api :: Proxy API
+api = Proxy
+
+-- | And now we implement the REST service by matching the API type, providing a Handler method for each endpoint
+-- defined in the API type. Normally we would implement each endpoint using a unique method definition, but this need
+-- not be so. To add a news endpoint, define it in type API above, and add and implement a handler here.
+server :: Server API
+server = hello
+     :<|> postRepoInfo
+
+  where
+        hello :: Maybe String -> Handler HelloMessage
+        hello mname = return . HelloMessage $ case mname of
+          Nothing -> "Hello, anonymous coward"
+          Just n  -> "Hello, " ++ n
+
+        postRepoInfo :: [RepoInfo] -> Handler RepoInfoResponse
+        postRepoInfo repos = liftIO $ do
+--             print "Hello world"
+            c <- getChar
+            let success = True
+            let xs = map repo_name repos
+                msg = "the mes is " ++ [c]
+
+            return $ RepoInfoResponse success
+
+-- Get User Github Info
+-- getUserGithubInfo :: Maybe String -> String
+-- getUserGithubInfo user = do
+--     return $ possibleUser $ GH.executeRequest' $ GH.userInfoForR user
+
+-- | error stuff
+custom404Error msg = err404 { errBody = msg }
+
+-- | Determines log reporting level. Set to "DEBUG", "WARNING" or "ERROR" as preferred. Loggin is
+-- provided by the hslogger library.
+logLevel :: IO String
+logLevel = defEnv "LOG_LEVEL" id "DEBUG" True
+
+-- | Logging stuff
+iso8601 :: UTCTime -> String
+iso8601 = formatTime defaultTimeLocale "%FT%T%q%z"
+
+-- global loggin functions
+debugLog, warnLog, errorLog :: String -> IO ()
+debugLog = doLog debugM
+warnLog  = doLog warningM
+errorLog = doLog errorM
+noticeLog = doLog noticeM
+
+doLog f s = getProgName >>= \ p -> do
+                t <- getCurrentTime
+                f p $ (iso8601 t) ++ " " ++ s
+
+withLogging act = withStdoutLogger $ \aplogger -> do
+
+  lname  <- getProgName
+  llevel <- logLevel
+  updateGlobalLogger lname
+                     (setLevel $ case llevel of
+                                  "WARNING" -> WARNING
+                                  "ERROR"   -> ERROR
+                                  _         -> DEBUG)
+  act aplogger
+
+
+-- | Helper function to simplify the setting of environment variables
+-- function that looks up environment variable and returns the result of running funtion fn over it
+-- or if the environment variable does not exist, returns the value def. The function will optionally log a
+-- warning based on Boolean tag
+defEnv :: Show a
+              => String        -- Environment Variable name
+              -> (String -> a)  -- function to process variable string (set as 'id' if not needed)
+              -> a             -- default value to use if environment variable is not set
+              -> Bool          -- True if we should warn if environment variable is not set
+              -> IO a
+defEnv env fn def doWarn = lookupEnv env >>= \ e -> case e of
+      Just s  -> return $ fn s
+      Nothing -> do
+        when doWarn (doLog warningM $ "Environment variable: " ++ env ++
+                                      " is not set. Defaulting to " ++ (show def))
+        return def
